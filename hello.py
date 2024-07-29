e9f3c8c6c3c71a24ddee1cea29f43082929ec628
PriyankarLavate@2005 <priyankalavate909@gmail.com>
2024-07-30 00:24:37 +0530

    First Commit

---
+++ hello.py
@@ -0,0 +1,78 @@
+# print(abs(-5))
+# print("power function",pow(2,3))
+# print("round function is",round(4.56))
+# num=[3,4,5,8,89]
+# print("minimum function",min(num))
+# print("maximum function",max(num))
+# name="priyanka Lavate"
+# print("string is",name[2])
+# sis=" \t pooja"
+# print("with sis",name+sis)
+# print(len(sis))
+# print(sis.capitalize())
+# print(sis.upper())
+# print(sis.lower())
+# # escape Character
+# print("priyanka\nLavate")
+# print("priyanka\tLavate")
+# print("priyanka\" Lavate\"")
+# #list programs
+# student=["priyanka","pooja","neha","shreepriya"]
+# print(student)
+# print(len(student))
+# print(student[1])
+# print(student[-1])
+# student.append("anurag sir")
+# print(student)
+# student.insert(2,"puja")
+# print(student)
+# teacher=["pdmane","sumit sir","vishwa sir","gaurav sir"]
+# student.extend(teacher)
+# print(student)
+# student.remove("priyanka")
+# print(student)
+# student.pop(2)
+# print(student)
+# student[2:8]="all deleted","try again"
+# print(student)
+# student.reverse()
+# print(student)
+# l2=student.copy()
+# print(l2)
+# t1=tuple(l2)
+# print(t1)
+# t3=[2,3,6,8]
+# t3.sort()
+# print(t3)
+# t3.sort(reverse=True)
+# print(t3)
+#fruits
+#tuple
+# fruits=("apple","mango","banana","chikoo","pinaple","orange")
+# print(len(fruits))
+# print(fruits.index("apple"))
+# print(fruits[3])
+# print(fruits[2:4])
+# print(type (fruits))
+###dictionary
+# stu_detail={'name':'priyanka',"name":"priyanka","surname":"lavate","roll no":24,"stream":"CSE"}
+# print(stu_detail['name'])
+# stu={'coa':60,'maths':65}
+# stu_detail.update(stu)
+# print(stu_detail)
+
+# stu_detail.popitem()
+# print(stu_detail)
+# stu_detail.copy()
+# del stu_detail['name']
+# print(stu_detail)
+# # print(stu_detail.clear)
+# set1={2,4,5,8,7,3,9}
+# print(set1)
+# print(set1.remove(5))
+# set1.add(67)
+# print(set1)
+for i in range(1,5):
+    name=input()
+    score = float(input())
+    
